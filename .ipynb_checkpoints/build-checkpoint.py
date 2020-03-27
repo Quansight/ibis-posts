2d7b2944e3442899b845be0ed93575ede707c3da
Tony Fast <tony.fast@gmail.com>
2020-03-27 13:57:38 -0400

    rm

--- .ipynb_checkpoints/build-checkpoint.py
+++
@@ -1,7 +0,0 @@
-import nbconvert, pathlib
-def export(In, Out):
-        pathlib.Path(Out).write_text(F"""---\n\n---\n
-        {nbconvert.MarkdownExporter(exclude_input=True).from_filename(In)[0]}
-        """);
-if __name__=='__main__':
-    export('ibis_posts/01.md.ipynb', '1.md')
