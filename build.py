aa336bcc7f891d926e88506f102a252b5263356f
Tony Fast <tony.fast@gmail.com>
2020-03-27 14:25:01 -0400

    b

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 import nbconvert, pathlib, yaml
 def export(In, Out, **globals):
         pathlib.Path(Out).write_text(F"""---\n{yaml.safe_dump(globals)}\n---
-    {nbconvert.MarkdownExporter(exclude_input=True).from_filename(In)[0]}""");
+{nbconvert.MarkdownExporter(exclude_input=True).from_filename(In)[0]}""");
 if __name__=='__main__':
     export('ibis_posts/01.md.ipynb', 'docs/1.md', permalink="/expressions/")
